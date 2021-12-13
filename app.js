ff8c15d10580c06055cd966051057aea299ba266
Fidel Quedet <38511646+quedet@users.noreply.github.com>
2021-12-13 00:39:18 +0000

    The view using express pug

---
+++ app.js
@@ -0,0 +1,28 @@
+// Third-Party Dependencies
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+
+// App Init
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// View Engine
+app.set('view engine', 'pug')
+app.set('views', path.resolve(__dirname, 'views'))
+
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+// Routing
+app.get('/', (req, res) => {
+    res.render('pages/home')
+})
+
+// Server Listening
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}; Press Ctrl+C to terminate...`);
+})
